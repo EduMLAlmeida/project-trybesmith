4fccd8784a7527bcca8792af743d15bdbadcea4b
EduMLAlmeida <edualmeidawebdev@gmail.com>
2022-10-06 16:56:20 -0300

    Criados connectio e usermodel.

---
+++ src/interfaces/index.ts
@@ -0,0 +1,14 @@
+export interface IUser {
+  id: number,
+  username: string,
+  class: string,
+  level: number,
+  password: string,
+}
+
+export interface IProduct {
+  id: number,
+  name: string,
+  amount: string,
+  orderid: number,
+}
