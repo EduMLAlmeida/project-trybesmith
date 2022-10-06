cef52ef7ce4d87f9b72e50a395d4a6afc7aae05f
EduMLAlmeida <edualmeidawebdev@gmail.com>
2022-10-06 17:41:17 -0300

    Implementado getall no productmodel.

--- src/models/ProductModel.ts
+++ src/models/ProductModel.ts
@@ -1,5 +1,10 @@
+import { RowDataPacket } from 'mysql2';
 import connection from './connection';
 
 export default class ProductModel {
-  
+  getAll = async () => {
+    const query = 'SELECT * FROM Trybesmith.Products';
+    const [products] = await connection.execute<RowDataPacket[]>(query);
+    return products;
+  };
 }
